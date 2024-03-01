295d1a35b0ca8edfd715835cc8ec7f96c6a35156
yazawang <pwh0808@naver.com>
2024-03-01 19:32:14 +0900

    study of java class and method

---
+++ src/study/class_01/Main.java
@@ -0,0 +1,10 @@
+package study.class_01;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Person p1 = new Person("yaza", 3, 0, new String[] {"h", "q", "df"});
+        p1.presentSelf();
+    }
+}
---
+++ src/study/class_01/Person.java
@@ -0,0 +1,76 @@
+package study.class_01;
+
+import java.util.Arrays;
+
+public class Person {
+    private String name;
+    private int age;
+    private int gender;
+    private String[] hobby;
+
+    // 아무것도 입력받지 않는 생성자
+    public Person() {
+    }
+
+    // 4개 변수에 대한 getter/setter
+    public Person(String name, int age, int gender, String[] hobby) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.hobby = hobby;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String[] getHobby() {
+        return hobby;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHobby(String[] hobby) {
+        this.hobby = hobby;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        String sex;
+        if (gender == 0) {
+            sex = "man";
+        } else if(gender == 1) {
+            sex = "woman";
+        } else {
+            sex = "trans";
+        }
+        return "study.class_01.Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + sex +
+                ", hobby=" + Arrays.toString(hobby) +
+                '}';
+    }
+
+    public void presentSelf() {
+        System.out.println(toString());
+    }
+}
